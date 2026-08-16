ef3941ff0e548e8217b446c316e9ed2fb17017e6
Matheus Duarte Antunes <80714618+DAMatheus@users.noreply.github.com>
2026-08-16 19:07:57 -0300

    criacao-bd-e-breeze

--- database/migrations/2026_08_16_163634_create_users_table.php
+++ database/migrations/2026_08_16_163634_create_users_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('user');
+            $table->foreignId('sector_id')
+            ->nullable()
+            ->constrained('sectors')
+            ->nullOnDelete();   
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@vshelp.com',
+            'password' => 'Admin@123',
+            'role' => 'super-admin',
+            'sector_id' => null
+        ]);
+    }
+}
